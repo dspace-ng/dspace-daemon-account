35bd23092a244d4c4169c7aa130b50cc63cc3c5e
elf Pavlik <perpetual-tripper@wwelves.org>
2013-09-18 16:50:32 +0200

    major #prune :)

--- README.md
+++ README.md
@@ -1,4 +1,8 @@
-== Config
+# DSpace Bayeux Server
 
+creates **pub/sub** server using [Bayeux](http://svn.cometd.com/trunk/bayeux/bayeux.html) protocol implemented by [Faye](http://faye.jcoglan.com/)
+
+    $ npm install
     $ cp config.json.example config.json
+    $ node server.js
 
--- server.js
+++ server.js
@@ -1,11 +1,6 @@
 var Faye = require('faye');
 var cradle = require('cradle');
-var http = require('http');
-var fs = require('fs');
 var nconf = require('nconf');
-var formidable = require('formidable');
-var util = require('util'); // #debug
-var static = require('node-static');
 
 /*
  * get config from file
@@ -24,22 +19,6 @@ if(nconf.get('couchdb').database !== "") {
                                   options).database(nconf.get('couchdb').database);
 }
 
-/*
- * static media files
- */
-var mediaPath = process.cwd() + '/upload';
-var mediaServer = new static.Server();
-
-
-var savedState = {};
-
-var notMeta = function(message){
-  return !message.channel.match(/^\/meta\/.*/);
-};
-
-var isSubscription = function(message){
-  return message.channel == '/meta/subscribe';
-};
 /*
  * Extension to persist data
  * http://faye.jcoglan.com/node/extensions.html
@@ -71,103 +50,9 @@ var persistData = {
   }
 };
 
-var rememberState = {
-  incoming: function(message, callback) {
-    if(notMeta(message)) {
-      var channel = message.channel.split('/')[2];
-      var uuid = message.channel.split('/')[3];
-      if(!savedState[uuid])
-        savedState[message.channel] = {};
-      savedState[uuid][message.nickname] = message;
-    }
-    callback(message);
-  },
-
-  outgoing: function(message, callback) {
-    if(isSubscription(message) && message.successful) {
-      if(! message.ext) message.ext = {};
-      if(message.subscription in savedState) {
-        var channelState = savedState[message.subscription];
-        message.ext.initialState = Object.keys(channelState).map(function(nickname) {
-          return channelState[nickname];
-        });
-      } else {
-        message.ext.initialState = [];
-      }
-    }
-    callback(message);
-  }
-};
-
 var bayeux = new Faye.NodeAdapter({mount: '/faye'});
 if(persisting) bayeux.addExtension(persistData);
-//bayeux.addExtension(rememberState);
 
-var CORS_HEADERS = {
-  'Access-Control-Allow-Origin': '*',
-  'Access-Control-Allow-Methods': 'POST',
-  'Access-Control-Allow-Headers': 'Origin, Content-Type',
-  'Access-Control-Expose-Headers': 'Content-Type, Content-Length'
-};
-
-
-var server = http.createServer(function(request, response) {
-
-  function saveAttachement(fields, files) {
-    var noteUUID = JSON.parse(fields.meta).noteUUID;
-    var destination = mediaPath + '/' +  noteUUID;
-
-    var is = fs.createReadStream(files.file.path);
-    var os = fs.createWriteStream(destination);
-
-    is.pipe(os);
-    is.on('end',function() {
-          fs.unlinkSync(files.file.path);
-    });
-  }
-
-  console.log('REQUEST', request.method, request.url);
-  switch(request.method) {
-    case 'GET':
-      if(request.url.match('/upload/*')){
-      fs.readFile(__dirname + request.url, function (err,data) {
-        if (err) {
-          response.writeHead(404, CORS_HEADERS);
-          response.end(JSON.stringify(err));
-          return;
-        }
-        response.writeHead(200, CORS_HEADERS);
-        response.end(data);
-      }
-                 );
-      }
-    break;
-
-
-    case 'OPTIONS':
-      response.writeHead(204, CORS_HEADERS);
-    response.end();
-    break;
-    case 'POST':
-      if(request.url == '/upload') {
-      var noteUUID;
-
-      var form = new formidable.IncomingForm();
-      form.parse(request, function(err, fields, files) {
-        response.writeHead(200, CORS_HEADERS);
-        response.write('received upload:\n\n');
-        response.end(saveAttachement(fields, files));
-      });
-      return;
-    }
-    break;
-  }
-
-});
-
-
-bayeux.attach(server);
 var port = nconf.get('faye').port;
-server.listen(port, function() {
-  console.log('listening on ' + port);
-});
+bayeux.listen(port);
+console.log('listening on ' + port);
